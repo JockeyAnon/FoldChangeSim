3fa2aecc0608347863183a403f1b26f8dabc1f69
JockeyAnon <danielpark151@gmail.com>
2024-07-22 09:15:24 -0700

    Updated to tackle desynchronized dynamics

--- NewSIm.py
+++ NewSIm.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 newdir = "C:/Users/Daniel Park/Desktop/SimGraphs/"
 
-for i in range(300):
+for i in range(100):
     umax = 2
     dc = random.uniform(0, random.uniform(0,2))
     ks = random.uniform(0, random.uniform(0,2))
@@ -21,27 +21,6 @@ for i in range(300):
 
     f = - (math.log(((1/0.995)-1),0.5))
 
-    def odes(x,t):
-
-        n=x[0]
-        u=x[1]
-        c=x[2]
-        s=x[3]
-        p=x[4]
-
-        dndt = u * (s/(ki+s))*n
-
-        dudt = umax * (s/(ks+s)) * ((c/cmax)**(-f))/((0.5**(-f))+((c/cmax)**(-f)))
-
-        # dcdt = -c*(u+(qmax/(c+ks+((c**2)/ki))))
-        dcdt = -c*(u+dp)
-
-        dsdt = -(g) * dndt
-
-        # dpdt = w * ((c/(c+1))) - p*(u+(qmax/(p+ks+((p**2)/ki))))
-        dpdt = w * ((c/(c+1))) - p*(u+dp)
-
-        return [dndt,dudt,dcdt,dsdt,dpdt]
 
     # initial condition 
     x0 = [0.1,1.7,0.1,0.05,0]
@@ -50,14 +29,29 @@ for i in range(300):
     # values of time 
     timespent = 10
     t = np.linspace(0,timespent,(timespent*50)+1) 
+
+    p = np.linspace(0,timespent,(timespent*50)+1)
+    n = np.linspace(0,timespent,(timespent*50)+1)
+    u = np.linspace(0,timespent,(timespent*50)+1) 
+    c = np.linspace(0,timespent,(timespent*50)+1) 
+    s = np.linspace(0,timespent,(timespent*50)+1)
+
+    p[0] = 0
+    n[0] = 0.55
+    u[0] = 1.7
+    c[0] = 0.1
+    s[0] = 0.05
+
+
+    for i in range((timespent*50)):
+
+        p[i+1] = p[i] + w * ((c[i]/(c[i]+1))) - p[i]*(u[i] * (s[i]/(ki+s[i]))*n[i]+dp)
+        n[i+1] = n[i] + u[i] * (s[i]/(ki+s[i]))*n[i]
+        u[i] = umax * (s[i]/(ks+s[i])) * ((c[i]/cmax)**(-f))/((0.5**(-f))+((c[i]/cmax)**(-f)))
+        c[i+1] = c[i] + -c[i]*(u[i] * (s[i]/(ki+s[i]))*n[i]+dp)
+        s[i+1] = s[i] + -(g) * u[i] * (s[i]/(ki+s[i]))*n[i]
     
-    # solving ODE 
-    x = odeint(odes,x0,t) 
-    n=x[:,0]
-    u=x[:,1]
-    c=x[:,2]
-    s=x[:,3]
-    p=x[:,4]
+    u[-1] = u[-2]
 
     # plt.plot(t,n, label="number of cells") 
     figure, axis = plt.subplots(2, 3) 
@@ -80,9 +74,7 @@ for i in range(300):
 
 
         
-    filename = newdir + 'sim pic with variables '+ "ki = "+ str(ki) +"ks = "+ str(ks) + "dc = "+ str(dc) + "g = "+ str(g) +"dp = "+ str(dp) + "qmax = "+ str(qmax) + '.png'
-    figure.savefig(filename)
-    plt.close(figure)
+    plt.show()
 
 
 
